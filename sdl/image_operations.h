120294549864cc846151ba59ff8b83335583f788
Hugo GIRARD <hugo.girard@epita.fr>
2020-10-27 16:10:47 +0100

    modif rot

---
+++ sdl/image_operations.h
@@ -0,0 +1,6 @@
+#ifndef IMAGE_OPERATIONS_H
+#define IMAGE_OPERATIONS_H
+
+SDL_Surface* SDL_Rotation(SDL_Surface * picture, float angle);
+
+#endif
---
+++ sdl/main.c
@@ -0,0 +1,29 @@
+#include "SDL2/SDL.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include "display_picture.h"
+#include "save_char_bitmap.h"
+#include "image_operations.h"
+
+int main(int argc, char **argv){
+    init();//init the lib sdl
+
+    if (argc < 2)//two args to start, die with an error message if not
+        errx(1, "Too few arguments");
+
+    SDL_Surface * picture;
+
+    char *path = argv[1];//get the path of the picture to display
+    picture = load_picture(path);//load the picture thanks to the path
+    display(picture, 0);//display the picture
+
+    if (argc == 3)
+    {
+        picture = SDL_Rotation(picture, atoi(argv[2]));
+        display(picture, 1);
+    }
+    SDL_FreeSurface(picture);//Free the memory used by the picture
+
+    return 0;
+}
